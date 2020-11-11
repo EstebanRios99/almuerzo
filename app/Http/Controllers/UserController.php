a1783af5fd12a0c580618d4e84d94e41da41db36
sistemas01 <esteban.rios@epn.edu.ec>
2020-11-11 11:27:24 -0500

    Configuración para la conexión

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\User;
 use Illuminate\Http\Request;
+use App\Http\Resources\UserCollection;
+use App\Http\Resources\User as UserResource;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 use JWTAuth;
@@ -16,54 +18,67 @@ class UserController extends Controller
         $credentials = $request->only('email', 'password');
         try {
             if (! $token = JWTAuth::attempt($credentials)) {
-                return response()->json(['error' => 'invalid_credentials'], 400);
+                return response()->json(['message' => 'invalid_credentials'], 400);
             }
         } catch (JWTException $e) {
-            return response()->json(['error' => 'could_not_create_token'], 500);
+            return response()->json(['message' => 'could_not_create_token'], 500);
         }
+        $user =JWTAuth::user();
         
-        return response()->json(compact('token'));
+        return response()->json(compact('token','user'));
     }
 
     public function register(Request $request)
     {
-        $validator = Validator::make($request->all(), [
+        $request->validate([
             'name' => 'required|string|max:255',
             'lastname' => 'required|string|max:255',
             'email' => 'required|string|email|max:255|unique:users',
             'password' => 'required|string|min:6|confirmed',
-            ]);
+        ]);
+                     
+        $user = User::create([
+            'name' => $request->get('name'),
+            'lastname' => $request->get('lastname'),
+            'email' => $request->get('email'),
+            'password' => Hash::make($request->get('password')),
+        ]);
             
-            if($validator->fails()){
-                return response()->json($validator->errors()->toJson(), 400);
-            }
+        $token = JWTAuth::fromUser($user);
             
-            $user = User::create([
-                'name' => $request->get('name'),
-                'lastname' => $request->get('lastname'),
-                'email' => $request->get('email'),
-                'password' => Hash::make($request->get('password')),
-                ]);
-                
-                $token = JWTAuth::fromUser($user);
-                
-                return response()->json(compact('user','token'),201);
-            }
+        return response()->json(new UserResource('user','token'),201);
+    }
             
-            public function getAuthenticatedUser()
-            {
-                try {
-                    if (! $user = JWTAuth::parseToken()->authenticate()) {
-                        return response()->json(['user_not_found'], 404);
-                    }
-                
-                } catch (Tymon\JWTAuth\Exceptions\TokenExpiredException $e) {
-                    return response()->json(['token_expired'], $e->getStatusCode());
-                } catch (Tymon\JWTAuth\Exceptions\TokenInvalidException $e) {
-                    return response()->json(['token_invalid'], $e->getStatusCode());
-                } catch (Tymon\JWTAuth\Exceptions\JWTException $e) {
-                    return response()->json(['token_absent'], $e->getStatusCode());
-                }
-                return response()->json(compact('user'));
+    public function getAuthenticatedUser()
+    {
+        try {
+            if (! $user = JWTAuth::parseToken()->authenticate()) {
+                return response()->json(['message'], 404);
             }
-}
+        
+        } catch (Tymon\JWTAuth\Exceptions\TokenExpiredException $e) {
+            return response()->json(['message'], $e->getStatusCode());
+        } catch (Tymon\JWTAuth\Exceptions\TokenInvalidException $e) {
+            return response()->json(['message'], $e->getStatusCode());
+        } catch (Tymon\JWTAuth\Exceptions\JWTException $e) {
+            return response()->json(['message'], $e->getStatusCode());
+        }
+        return response()->json(new UserResource($user),200);
+    }
+
+    public function logout() 
+    { 
+        try { 
+            JWTAuth::invalidate(JWTAuth::getToken());
+            
+            return response()->json([ 
+                "status" => "success",
+                 "message" => "User successfully logged out." 
+                ], 200);
+             } catch (JWTException $e) { 
+                 // something went wrong whilst attempting to encode the token 
+                 return response()->json(["message" => "No se pudo cerrar la sesión."
+                ], 500); 
+            }
+        }
+    }
--- routes/api.php
+++ routes/api.php
@@ -13,24 +13,31 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::post('register', 'UserController@register');
-Route::post('login', 'UserController@authenticate');
 
+Route::group(['middleware' => ['cors']], function () {
 
+    Route::post('register', 'UserController@register');
+    Route::post('login', 'UserController@authenticate');
 
-Route::group(['middleware' => ['jwt.verify']], function() {
+    Route::group(['middleware' => ['jwt.verify']], function() {
 
-    Route::get('employs', 'EmployController@index');
-    Route::get('employs/{employ}', 'EmployController@show');
-    Route::get('search/employ/{identification}', 'EmployController@searchEmploy');
-    Route::post('employs', 'EmployController@store');
-    Route::put('employs/{employ}', 'EmployController@update');
-    Route::delete('employs/{employ}', 'EmployController@delete');
+        Route::get('user', 'UserController@getAuthenticatedUser'); 
+        Route::post('logout', 'UserController@logout');
+        
+        //employs
+        Route::get('employs', 'EmployController@index');
+        Route::get('employs/{employ}', 'EmployController@show');
+        Route::get('search/employ/{identification}', 'EmployController@searchEmploy');
+        Route::post('employs', 'EmployController@store');
+        Route::put('employs/{employ}', 'EmployController@update');
+        Route::delete('employs/{employ}', 'EmployController@delete');
 
-    Route::get('employs/{identification}/registers','EmployController@registersByEmploy');
-    Route::get('registers', 'RegisterController@index');
-    Route::get('registers/{register}', 'RegisterController@show');
-    Route::post('registers', 'RegisterController@store');
-    Route::put('registers/{register}', 'RegisterController@update');
-    Route::delete('registers/{register}', 'RegisterController@delete');
+        //registers
+        Route::get('employs/{identification}/registers','EmployController@registersByEmploy');
+        Route::get('registers', 'RegisterController@index');
+        Route::get('registers/{register}', 'RegisterController@show');
+        Route::post('registers', 'RegisterController@store');
+        Route::put('registers/{register}', 'RegisterController@update');
+        Route::delete('registers/{register}', 'RegisterController@delete');
+    });
 });
